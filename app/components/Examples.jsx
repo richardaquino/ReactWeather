ab4cbad75c2fa7e93d687bdabba0dcf682bff830
Richard Aquino <richard.aquino@archibus-phil.com>
2020-03-01 18:56:00 +0800

    added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
-        <h3>Examples Component!</h3>
+        <div>
+            <h3>Examples Component!</h3>
+            <h3>Welcome to example pages</h3>
+        </div>
     );
 }
 
