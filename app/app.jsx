f0c7d1d4d9e2f15b92a7e5cb4cb89128859278b7
Richard Aquino <richard.aquino@archibus-phil.com>
2020-03-02 19:24:11 +0800

    Add Error Modal

--- app/app.jsx
+++ app/app.jsx
@@ -8,6 +8,7 @@ var Examples = require('Examples');
 
 //Load Foundation
 require('style!css!foundation-sites/dist/css/foundation.min.css');
+require('style!css!foundation-sites/dist/css/foundation-float.min.css');
 $(document).foundation();
 
 ReactDOM.render(
